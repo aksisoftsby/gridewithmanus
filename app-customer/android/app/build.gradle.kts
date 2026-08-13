1470bed9d1849ac0105fee57a8aca46ff67d9996
aksisoftsby <3.0791021e+07+aksisoftsby@users.noreply.github.com>
2026-08-13 05:52:06 +0000

    Add kotlinOptions jvmTarget 17 to fix Kotlin/JVM inconsistency on build

--- app-customer/android/app/build.gradle.kts
+++ app-customer/android/app/build.gradle.kts
@@ -15,6 +15,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.gride.app"
